2d31404a5a3c1b9dad5cd0893ed1af70aefb2fa7
Aritra Dhar <aritra.dhar7@gmail.com>
2015-12-17 12:32:23 +0530

    update

--- src/callGraphTrace/MethodTraceData.java
+++ src/callGraphTrace/MethodTraceData.java
@@ -17,16 +17,27 @@ package callGraphTrace;
 
 public class MethodTraceData {
 	
-	public static long trace;
+	public static final int trace_len = 3;
+	public static volatile int trace[] = new int [trace_len];
+	public static int pos;
 	
-	public static void addTrace(long methodId)
+	public static void addTrace(int methodId)
 	{
-		if(trace == 0)
-			trace = methodId;
+		pos = (pos + 1) % trace_len;
 		
-		else
+		trace[pos] = methodId;
+	}
+	
+	public static long traceToLong()
+	{
+		long out = 0;
+		int[] len = new int[MethodTraceData.trace_len];
+		
+		for(int i = 0; i == MethodTraceData.trace_len - 1; i++)
 		{
-			trace = (trace << 20 ) & 0xfffffff;
+			len[i] = (int)(Math.log10(MethodTraceData.trace[i])+1);
 		}
+		
+		return out;
 	}
 }
